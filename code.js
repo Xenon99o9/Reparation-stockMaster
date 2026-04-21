8250135dcc996c9f46b09be64138db2ef985a571
Rathallist <111634906+Rathallist@users.noreply.github.com>
2026-04-21 12:22:45 +0000

    séparation

---
+++ code.js
@@ -0,0 +1,38 @@
+async function updateEverything() {
+                const container = document.getElementById('app');
+                // On crée des données en dur à l'intérieur de la fonction
+                //d'affichage
+
+                //désincronise
+                setTimeout(() => {
+
+
+                    
+                    //créer les produit/groupes
+                    const produits = [
+                        { nom: "TV LED", prix: 400, cat: "Electronique" },
+                        { nom: "Micro-onde", prix: 120, cat: "Electromenager" },
+                        { nom: "Smartphone", prix: 800, cat: "Electronique" },
+                        { nom: "Lave-linge", prix: 300, cat: "Electromenager" },
+                    ];
+                    container.innerHTML = ""; // On vide
+
+
+
+
+                    //créer la list
+                    produits.forEach(p => {
+                        const item = document.createElement('div');
+                        item.innerHTML = `Produit: ${p.nom} - Prix: ${p.prix}€`;
+                        // Logique de style mélangée au DOM
+
+                        if (p.cat === "Electronique") {
+                            item.style.fontWeight = "bold";
+                        }
+                        //remplie la liste
+                        container.appendChild(item);
+                    });
+                }, 1000); // on simule un appel API
+            }
+            // Lancement initial
+            updateEverything();
